8bd469a4bf65e69b2acf64e0a900527f716fa5fa
Chip Nielsen <chip.nielsen@networktocode.com>
2021-06-08 16:27:05 -0700

    Initial commit of Nautobot DiffSync adapter

---
+++ aristacv_sync/diffsync/nautobot.py
@@ -0,0 +1,36 @@
+"""DiffSync adapter for Nautobot."""
+from nautobot.dcim.models import Device as NautobotDevice
+from nautobot.extras.models.tags import Tag
+from diffsync import DiffSync
+
+from .models import Device, Tag
+
+
+class Nautobot(DiffSync):
+    """DiffSync adapter implementation for Nautobot user-defined tags."""
+
+    device = Device
+    tag = Tag
+
+    top_level = ["device"]
+
+    type = "Nautobot"
+
+    nb = None
+
+    def load(self):
+        """Load device tag data from Nautobot and populate DiffSync models."""
+        devices = NautobotDevice.objects.all()
+        for dev in devices:
+            self.device = Device(name=dev.name, device_id=dev.serial)
+            self.add(self.device)
+            dev_tags = dev.tags.all()
+            for tag in dev_tags:
+                if ":" in tag.name:
+                    label, value = tag.name.split(":")
+                else:
+                    label = tag.name
+                    value = None
+                self.tag = Tag(name=label, device_name=dev.name, value=value, tag_type="CREATOR_TYPE_USER")
+                self.add(self.tag)
+                self.device.add_child(self.tag)
