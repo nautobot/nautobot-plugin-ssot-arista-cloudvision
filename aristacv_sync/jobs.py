c2b69535c42fc8f58b89be9bc46490bbca2d5615
Chip Nielsen <chip.nielsen@networktocode.com>
2021-06-08 16:40:42 -0700

    Add diff logic to sync to CV job

--- aristacv_sync/jobs.py
+++ aristacv_sync/jobs.py
@@ -1,6 +1,7 @@
 from nautobot.extras.jobs import Job, BooleanVar
 
 from aristacv_sync.diffsync.cloudvision import CloudVision
+from aristacv_sync.diffsync.nautobot import Nautobot
 
 
 class FormEntry:
@@ -28,9 +29,18 @@ class CVSyncToJob(Job, FormEntry):
         description = "Sync custom tag data from Nautobot to CloudVision"
 
     def run(self, data, commit):
-        cv = CloudVision()
         self.log("Loading data from CloudVision")
+        cv = CloudVision()
         cv.load()
+        self.log("Loading data from Nautobot")
+        nb = Nautobot()
+        nb.load()
+        self.log("Performing diff between Nautobot and Cloudvision")
+        diff_nb_cv = nb.diff_from(cv)
+        self.log("Diff:")
+        self.log(diff_nb_cv.str())
+        if commit:
+            self.log("Syncing to CloudVision")
 
 
 jobs = [CVSyncFromJob, CVSyncToJob]
