e875a4483fdf3aa45fa32e9a56f376a99f5b57c0
Chip Nielsen <chip.nielsen@networktocode.com>
2021-06-15 06:43:40 -0700

    Update loading for grpc refactor

--- aristacv_sync/diffsync/tocv/cloudvision.py
+++ aristacv_sync/diffsync/tocv/cloudvision.py
@@ -20,12 +20,13 @@ class CloudVision(DiffSync):
 
     def load(self):
         devices = cvutils.get_devices()
+        user_tags = cvutils.get_tags_by_type()
         for dev in devices:
             self.device = Device(name=dev["hostname"], device_id=dev["device_id"])
             self.add(self.device)
-            dev_tags = cvutils.get_device_tags(device_id=dev["device_id"])
+            # Filter device tags to user-defined tags only
+            dev_tags = [tag for tag in cvutils.get_device_tags(device_id=dev["device_id"]) if tag in user_tags]
             for tag in dev_tags:
-                if tag["type"] == "CREATOR_TYPE_USER":
-                    self.tag = UserTag(name=tag["label"], device_name=dev["hostname"], value=tag["value"])
-                    self.add(self.tag)
-                    self.device.add_child(self.tag)
+                self.tag = UserTag(name=tag["label"], device_name=dev["hostname"], value=tag["value"])
+                self.add(self.tag)
+                self.device.add_child(self.tag)
