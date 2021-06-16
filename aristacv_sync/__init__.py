0cd1c4eab6b354a1a964404bfe9c4c2d51182565
Adam Byczkowski <adam.byczkowski@networktocode.com>
2021-06-16 14:50:20 -0500

    Updated

--- aristacv_sync/__init__.py
+++ aristacv_sync/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "0.1.0"
 
 from nautobot.extras.plugins import PluginConfig
+from django.db.models.signals import post_migrate
 
 class AristaCVSyncConfig(PluginConfig):
     """Plugin configuration for the aristacv_sync plugin."""
@@ -22,81 +23,13 @@ class AristaCVSyncConfig(PluginConfig):
     def ready(self):
         """Callback invoked after the plugin is loaded."""
         super().ready()
+        print("ready was called.")
 
-        from nautobot.extras.choices import CustomFieldTypeChoices
-        from nautobot.dcim.models import Device
-        from nautobot.extras.models import CustomField
-        from django.contrib.contenttypes.models import ContentType
+        from .signals import (  # pylint: disable=import-outside-toplevel
+            post_migrate_create_custom_fields,
+        )
 
-        for device_cf_dict in [
-                {
-                    "name": "eos_train",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "EOS Train",
-                },
-                {
-                    "name": "eos_version",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "EOS Version",
-                },
-                {
-                    "name": "ztp",
-                    "type": CustomFieldTypeChoices.TYPE_BOOLEAN,
-                    "label": "ztp",
-                },
-                {
-                    "name": "pimbidir",
-                    "type": CustomFieldTypeChoices.TYPE_BOOLEAN,
-                    "label": "pimbidir",
-                },
-                {
-                    "name": "pim",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "pim",
-                },
-                {
-                    "name": "bgp",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "bgp",
-                },
-                {
-                    "name": "mpls",
-                    "type": CustomFieldTypeChoices.TYPE_BOOLEAN,
-                    "label": "mpls",
-                },
-                {
-                    "name": "serial_number",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "Serial Number",
-                },
-                {
-                    "name": "systype",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "systype",
-                },
-                {
-                    "name": "mlag",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "MLAG",
-                },
-                {
-                    "name": "tapagg",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "TAP Aggregation",
-                },
-                {
-                    "name": "sflow",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "sFlow",
-                },
-                {
-                    "name": "terminatr",
-                    "type": CustomFieldTypeChoices.TYPE_TEXT,
-                    "label": "TerminAttr Version",
-                },
-            ]:
-                field, _ = CustomField.objects.get_or_create(name=device_cf_dict["name"], defaults=device_cf_dict)
-                field.content_types.set([ContentType.objects.get_for_model(Device)])
+        post_migrate.connect(post_migrate_create_custom_fields)
 
 
 
