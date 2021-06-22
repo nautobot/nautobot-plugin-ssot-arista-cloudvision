fe5e2a386ddf080f9bd3570e9da8b5cc484e1b32
Adam Byczkowski <adam.byczkowski@networktocode.com>
2021-06-21 22:46:55 -0500

    fromcv working

--- aristacv_sync/jobs.py
+++ aristacv_sync/jobs.py
@@ -4,28 +4,56 @@ from django.templatetags.static import static
 
 from nautobot.extras.jobs import Job, BooleanVar
 from nautobot.extras.models.tags import Tag
+from nautobot.extras.models.customfields import CustomField
 
 from nautobot_ssot.jobs.base import DataTarget
+from nautobot_ssot.jobs.base import DataSource
 
 from aristacv_sync.diffsync.tocv.cloudvision import CloudVision
 from aristacv_sync.diffsync.tocv.nautobot import Nautobot
 
+from aristacv_sync.diffsync.fromcv.cloudvision import CloudVision as C
+from aristacv_sync.diffsync.fromcv.nautobot import Nautobot as N
 
-class FormEntry:
+class CloudVisionDataSource(DataSource, Job):
     debug = BooleanVar(description="Enable for more verbose debug logging")
-
-
-class CVSyncFromJob(Job, FormEntry):
-    debug = FormEntry.debug
 
     class Meta:
         name = "Sync from CloudVision"
+        data_source = "Cloudvision"
+        data_source_icon = static("aristacv_sync/cvp_logo.png")
         description = "Sync system tag data from CloudVision to Nautobot"
 
-    def run(self, data, commit):
-        cv = CloudVision()
+    def sync_data(self):
         self.log("Loading data from CloudVision")
+        cv = C()
         cv.load()
+        self.log("Loading data from Nautobot")
+        nb = N()
+        nb.load()
+        self.log("Performing diff between Cloudvision and Nautobot.")
+        diff = nb.diff_from(cv)
+        self.sync.diff = diff.dict()
+        self.sync.save()
+        self.log(diff.summary())
+        if not self.kwargs["dry_run"]:
+            self.log("Syncing to Nautbot")
+            try:
+                cv.sync_to(nb)
+            except RpcError as e:
+                self.log_failure("Sync failed.")
+                raise e
+            self.log_success(message="Sync complete.")
+
+    def lookup_object(self, model_name, unique_id):
+        if model_name == "cf":
+            try:
+                tag_name, value = unique_id.split("__")
+                return (Tag.objects.get(name=f"{tag_name}:{value}"), None)
+            except Tag.DoesNotExist:
+                pass
+        return (None, None)
+
 
 
 class CloudVisionDataTarget(DataTarget, Job):
@@ -63,11 +91,11 @@ class CloudVisionDataTarget(DataTarget, Job):
     def lookup_object(self, model_name, unique_id):
         if model_name == "tag":
             try:
-                tag_name, value = unique_id.split("__")
-                return (Tag.objects.get(name=f"{tag_name}:{value}"), None)
-            except Tag.DoesNotExist:
+                cf_name, value = unique_id.split("__")
+                return (CustomField.objects.get(name=f"{cf_name}"), None)
+            except CustomField.DoesNotExist:
                 pass
         return (None, None)
 
 
-jobs = [CVSyncFromJob, CloudVisionDataTarget]
+jobs = [CloudVisionDataSource, CloudVisionDataTarget]
