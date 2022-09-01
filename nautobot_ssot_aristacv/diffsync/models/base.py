5c2462c0e3dcf4508b62277be69ab5a28d80d235
Justin Drew <jdrew82@users.noreply.github.com>
2022-09-01 12:44:54 -0500

    feat: ✨ Add Port DiffSyncModel for interface syncing

--- nautobot_ssot_aristacv/diffsync/models/base.py
+++ nautobot_ssot_aristacv/diffsync/models/base.py
@@ -19,9 +19,31 @@ class Device(DiffSyncModel):
     device_model: str
     serial: str
     cfs: List["CustomField"] = list()
+    ports: List["Port"] = list()
     uuid: Optional[UUID]
 
 
+class Port(DiffSyncModel):
+    """Port Model."""
+
+    _modelname = "port"
+    _identifiers = (
+        "name",
+        "device",
+    )
+    _attributes = (
+        "speed",
+        "duplex",
+        "status",
+    )
+    _children = {}
+
+    name: str
+    device: str
+    type: Optional[str]
+    speed: Optional[str]
+    duplex: Optional[str]
+    status: Optional[str]
     uuid: Optional[UUID]
 
 
