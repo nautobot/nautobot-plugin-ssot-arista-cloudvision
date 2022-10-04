4a3f90d02ed62a1378572c7f675715a525057082
Justin Drew <jdrew82@users.noreply.github.com>
2022-10-04 15:23:29 -0500

    fix: Update methods to not come from Api class

--- nautobot_ssot_aristacv/diffsync/adapters/cloudvision.py
+++ nautobot_ssot_aristacv/diffsync/adapters/cloudvision.py
@@ -6,7 +6,7 @@ from diffsync.exceptions import ObjectAlreadyExists
 import distutils
 
 from nautobot_ssot_aristacv.diffsync.models.cloudvision import CloudvisionDevice, CloudvisionCustomField
-from nautobot_ssot_aristacv.utils.cloudvision import CloudvisionApi
+from nautobot_ssot_aristacv.utils import cloudvision
 
 
 class CloudvisionAdapter(DiffSync):
@@ -17,7 +17,7 @@ class CloudvisionAdapter(DiffSync):
 
     top_level = ["device"]
 
-    def __init__(self, *args, job=None, conn: CloudvisionApi, **kwargs):
+    def __init__(self, *args, job=None, conn: cloudvision.CloudvisionApi, **kwargs):
         """Initialize the CloudVision DiffSync adapter."""
         super().__init__(*args, **kwargs)
         self.job = job
@@ -25,8 +25,8 @@ class CloudvisionAdapter(DiffSync):
 
     def load(self):
         """Load tag data from CloudVision."""
-        devices = self.conn.get_devices()
-        system_tags = self.conn.get_tags_by_type(TAG.models.CREATOR_TYPE_SYSTEM)
+        devices = cloudvision.get_devices(client=self.conn)
+        system_tags = cloudvision.get_tags_by_type(TAG.models.CREATOR_TYPE_SYSTEM)
 
         for dev in devices:
             new_device = None
@@ -49,7 +49,11 @@ class CloudvisionAdapter(DiffSync):
                 self.job.log_warning(message=f"Device {dev} is missing hostname so won't be imported.")
                 continue
 
-            dev_tags = [tag for tag in self.conn.get_device_tags(device_id=dev["device_id"]) if tag in system_tags]
+            dev_tags = [
+                tag
+                for tag in cloudvision.get_device_tags(client=self.conn, device_id=dev["device_id"])
+                if tag in system_tags
+            ]
 
             # Check if topology_type tag exists
             list_of_tag_names = [value["label"] for value in dev_tags]
