3381340b70b8942c274b0a507132d3e01588cef7
Justin Drew <jdrew82@users.noreply.github.com>
2022-10-12 08:52:15 -0500

    fix: Check for mediaType key

--- nautobot_ssot_aristacv/utils/cloudvision.py
+++ nautobot_ssot_aristacv/utils/cloudvision.py
@@ -572,7 +572,9 @@ def get_interface_transceiver(client: CloudvisionApi, dId: str, interface: str):
 
     for batch in client.get(query):
         for notif in batch["notifications"]:
-            if notif["updates"].get("actualIdEepromContents"):
+            if notif["updates"].get("actualIdEepromContents") and notif["updates"]["actualIdEepromContents"].get(
+                "mediaType"
+            ):
                 return notif["updates"]["actualIdEepromContents"]["mediaType"]
             if notif["updates"].get("localMediaType"):
                 return notif["updates"]["localMediaType"]["Name"]
