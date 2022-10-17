1595f8f68604025f46bd95e6151e3ba4e5cd168b
Justin Drew <jdrew82@users.noreply.github.com>
2022-10-17 16:52:23 -0500

    fix: ♻️ Redo how Nautobot adapter load is performed. Fixes issues #93 and #95.
    I found that in an update run that the Nautobot adapter was returning no Devices. In my investigation it appears it was being caught in the try/except block for some AttributeError. I've refactored this to narrow down the block to just CustomField loading and have some logging if encountered.

--- nautobot_ssot_aristacv/diffsync/adapters/nautobot.py
+++ nautobot_ssot_aristacv/diffsync/adapters/nautobot.py
@@ -2,7 +2,7 @@
 from nautobot.dcim.models import Device as OrmDevice
 from nautobot.dcim.models import Interface as OrmInterface
 from diffsync import DiffSync
-from diffsync.exceptions import ObjectNotFound
+from diffsync.exceptions import ObjectNotFound, ObjectAlreadyExists
 
 from nautobot_ssot_aristacv.diffsync.models.nautobot import NautobotDevice, NautobotCustomField, NautobotPort
 from nautobot_ssot_aristacv.utils import nautobot
@@ -24,31 +24,34 @@ class NautobotAdapter(DiffSync):
 
     def load(self):
         """Load device custom field data from Nautobot and populate DiffSync models."""
-        devices = OrmDevice.objects.all()
-        for dev in devices:
+        for dev in OrmDevice.objects.filter(device_type__manufacturer__slug="arista"):
             try:
-                if dev.device_type.manufacturer.name.lower() == "arista":
-                    new_device = self.device(
-                        name=dev.name,
-                        device_model=dev.device_type.name,
-                        serial=dev.serial,
-                        version=nautobot.get_device_version(dev),
-                        uuid=dev.id,
-                    )
-                    self.add(new_device)
-                    dev_custom_fields = dev.custom_field_data
-
-                    for cf_name, cf_value in dev_custom_fields.items():
-                        if cf_value is None:
-                            cf_value = ""
-                        new_cf = self.cf(name=cf_name, value=cf_value, device_name=dev.name)
-                        self.add(new_cf)
-
-                    # Gets model from device and puts it into CustomField Object.
-                    new_cf = self.cf(name="arista_model", value=str(dev.platform), device_name=dev.name)
-                    self.add(new_cf)
-            except AttributeError:
+                new_device = self.device(
+                    name=dev.name,
+                    device_model=dev.device_type.name,
+                    serial=dev.serial,
+                    version=nautobot.get_device_version(dev),
+                    uuid=dev.id,
+                )
+                self.add(new_device)
+            except ObjectAlreadyExists as err:
+                self.job.log_warning(message=f"Unable to load {dev.name} as it appears to be a duplicate. {err}")
                 continue
+
+            for cf_name, cf_value in dev.custom_field_data.items():
+                if cf_name.starts_with("arista_"):
+                    try:
+                        new_cf = self.cf(
+                            name=cf_name, value=cf_value if cf_value is not None else "", device_name=dev.name
+                        )
+                        self.add(new_cf)
+                    except AttributeError as err:
+                        self.job.log_warning(message=f"Unable to load {cf_name}. {err}")
+                        continue
+
+                # Gets model from device type and puts it into CustomField Object.
+                new_cf = self.cf(name="arista_model", value=str(dev.device_type.name), device_name=dev.name)
+                self.add(new_cf)
 
         for intf in OrmInterface.objects.all():
             new_port = self.port(
